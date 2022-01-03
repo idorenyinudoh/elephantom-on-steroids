c9c81905db1458c0e9411f18f632ddae4b622457
Idorenyin Udoh <idorenyinudoh10@outlook.com>
2022-01-03 14:54:49 +0100

    feat: create heading

---
+++ components/TheHeading.vue
@@ -0,0 +1,40 @@
+<template>
+  <h1>
+    Acumen Digital Interview Task / Elephantom
+    <span v-if="elephantIsVisible">
+      <svg width="12" height="14" viewBox="0 0 12 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M1 1L10 6.56616L1 13" stroke="#919EAB" stroke-width="2"/>
+      </svg>
+      Elephant
+    </span>
+  </h1>
+</template>
+
+<script>
+export default {
+  props: {
+    elephantIsVisible: {
+      type: Boolean,
+      required: false,
+      default: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  margin: 0;
+  padding: 34px 35px 31px;
+  font-size: 18px;
+  font-weight: normal;
+  line-height: 28px;
+  color: #30425A;
+  background-color: #FFF;
+  box-shadow: 0 -7px 24px #00000040;
+}
+
+svg {
+  margin: 0 14px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,37 @@
 <template>
   <div class="container">
-    <TheMenu />
+    <TheMenu @action="toggleDisplay" />
+    <main>
+      <TheHeading :elephant-is-visible="!homeIsActive" />
+    </main>
   </div>
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
+  data() {
+    return {
+      homeIsActive: true
+    }
+  },
+  methods: {
+    toggleDisplay(button) {
+      button === 'home' ? this.homeIsActive = true : this.homeIsActive = false
+    }
+  }
 }
 </script>
+
+<style scoped>
+@media screen and (max-width: 999px) {
+  main {
+    margin-top: calc(100px + 1vw);
+  }
+}
+@media screen and (min-width: 1000px) {
+  main {
+    margin-left: 361px;
+  }
+}
+</style>
